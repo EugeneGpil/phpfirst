b19bb959eb3514102c79aff5b3d98de80351027b
EugeneGpil <gpilligrim@gmail.com>
2019-06-03 22:05:44 +0400

    while

--- includes/config.php
+++ includes/config.php
@@ -2,6 +2,11 @@
   $config = array(
     'title' => 'Блог IT_Минималиста',
     'vk_url' => 'https://vk.com/id1637050',
+    'main_page_categories' => array(
+      'безопасность',
+      'спорт',
+      'автомобили'
+    ),
     'db' => array(
       'server' => 'localhost',
       'username' => 'root',
