09d8722e1d609261c76f1a000a85eda1b6920abc
EugeneGpil <gpilligrim@gmail.com>
2019-06-08 21:45:20 +0400

    fixed footer buttons

--- includes/footer.php
+++ includes/footer.php
@@ -3,13 +3,13 @@
     <a href="/" class="logo"><?=$config['title']?></a>
     <nav class="running-title-navigation">
       <ul class="running-title-navigation__list">
-        <li><a href="/" class="running-title-navigation__button">главная</a></li>
+        <li><a href="/" class="running-title-navigation__button running-title-navigation__button_footer">главная</a></li>
         <li><a 
           href="<?=$config['vk_url']?>" 
-          class="running-title-navigation__button running-title-navigation__main-button"
+          class="running-title-navigation__button running-title-navigation__button_footer running-title-navigation__main-button"
           target="_blank">мы вконтакте</a></li>
-        <li><a href="/pages/info.php?id=1" class="running-title-navigation__button">об авторе</a></li>
-        <li><a href="/pages/info.php?id=2" class="running-title-navigation__button">правообладателям</a></li>
+        <li><a href="/pages/info.php?id=1" class="running-title-navigation__button running-title-navigation__button_footer">об авторе</a></li>
+        <li><a href="/pages/info.php?id=2" class="running-title-navigation__button running-title-navigation__button_footer">правообладателям</a></li>
       </ul>
     </nav>
   </div>
