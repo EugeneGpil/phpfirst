567beb88359801a87c4225d002768987aea78e80
EugeneGpil <gpilligrim@gmail.com>
2019-06-12 19:44:03 +0400

    content/article.php not finished

--- includes/content.php
+++ includes/content.php
@@ -8,11 +8,16 @@
     $toArticlesUrl = "/articles";
     // if uri begins with '/articles' 
     if (strrpos($uri, $toArticlesUrl) === 0){
-      //---------------------------------------------------
+      //================================================================
+      //================================================================
+      //for working with 'content/articles.php need to recognize
+      //$category and $page
+      //-------------------------------------------------------
       // if uri '/articles' or '/articles/' or 'articles/page',
       // go to 'contetnt/articles.php' with $category='' and $page=1
       $category = '';
       $page = 1;
+      $thisUrlforArticlesPage = true;
       //----------------------------------------------------
       // if uri '/articles/page%something%', $page = %something% and go to 'content/articles.php'
       $uri = substr($uri, strlen($toArticlesUrl));
@@ -21,7 +26,6 @@
       else{
         //--------------------------------------------------
         //need to find $someUrl for compare this with articles_categories url
-        //or with articles url
         //'articles/  %$someUrl%  /someElseUrl'
         $someUrl = $uri;
         if (substr($someUrl, 0, 1) == '/'){
@@ -48,9 +52,14 @@
           $uri = substr($uri, strlen($category));
           if (findPage($uri))
             $page = findPage($uri);
+          //---------------------------------------------------
         }
+        else $thisUrlforArticlesPage = false;
       }
-      include 'content/articles.php';
+      if ($thisUrlforArticlesPage) include 'content/articles.php';
+      else include 'content/article.php';
+      //================================================================
+      //================================================================
     }
   }
 ?>
--- includes/content/articles.php
+++ includes/content/articles.php
@@ -1,21 +1,38 @@
 <section id="start-of-articles-previews" class="main-content">
   <?php
-    $articles = $connection->query(
-      "SELECT t1.*, t2.id category_id, t2.title category_title 
-      FROM articles t1 
-      LEFT JOIN articles_categories t2 
-      ON t1.categories_id = t2.id 
-      ORDER BY `id` 
-      DESC"
-    );
-    contentSectionHeader("новейшее в блоге", "/articles/page1", "все записи");
+    if ($category == ''){
+      $articles = $connection->query(
+        "SELECT t1.*, t2.id category_id, t2.title category_title 
+        FROM articles t1 
+        LEFT JOIN articles_categories t2 
+        ON t1.categories_id = t2.id 
+        ORDER BY `id` 
+        DESC"
+      );
+      $sectionHeadline = "новейшее в блоге";
+    }
+    else{
+      $articles = $connection->query(
+        "SELECT t1.*,
+          t2.id category_id, t2.title category_title, t2.url category_url
+        FROM articles t1
+        LEFT JOIN articles_categories t2 
+        ON t1.categories_id = t2.id
+        WHERE t2.url = '$category'
+        ORDER BY `id` 
+        DESC"
+      );
+    }
+    $articles = $articles->fetchAll();
+    if ($category != '')
+    $sectionHeadline = $articles[0]['category_title'];
+    contentSectionHeader($sectionHeadline, $toArticlesUrl. $category, "все записи");
     $articlesPerPage = $config['articles_per_page'];
     $firstShownArticle = ($page - 1) * $articlesPerPage;
-    $articles = $articles->fetchAll();
     showArticles($articles, $articlesPerPage, $firstShownArticle);
   ?>
   <div class="main-content__change-page-container">
-    <a href="<?=$toArticlesUrl?>/page<?=$page - 1?>#start-of-articles-previews"
+    <a href="<?=$toArticlesUrl?><?=$category?>/page<?=$page - 1?>#start-of-articles-previews"
       class="main-content__page-change-button 
         main-content__page-change-element
         <?php
@@ -26,7 +43,7 @@
       <--
     </a>
     <div class="main-content__corrent-page main-content__page-change-element"><?=$page?></div>
-    <a href="<?=$toArticlesUrl?>/page<?=$page + 1?>#start-of-articles-previews"
+    <a href="<?=$toArticlesUrl?><?=$category?>/page<?=$page + 1?>#start-of-articles-previews"
       class="main-content__page-change-button
       main-content__page-change-element
         <?php
