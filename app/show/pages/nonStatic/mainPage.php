01ef21e9abf48b558a51895def24a6ce0fc0ecd5
EugeneGpil <gpilligrim@gmail.com>
2019-06-19 02:31:35 +0400

    main page articles by category

--- app/show/pages/nonStatic/mainPage.php
+++ app/show/pages/nonStatic/mainPage.php
@@ -23,7 +23,7 @@
             </a>
             <div class="article-preview__category article-preview__interactive-button-container">
               <span class="article-preview__category">категория:</span>
-              <a href="<?= '/' . $urls['articles'] . '/' . $article['category_url'] ?>/page1" class="article-preview__category article-preview__interactive-button"><?= $article['category_title'] ?>
+              <a href="<?= '/' . $urls['articles'] . '/' . $article['category_url'] ?>" class="article-preview__category article-preview__interactive-button"><?= $article['category_title'] ?>
               </a>
             </div>
             <a href="<?= $toArticleUrl ?>" class="article-preview__text content__button_grey-theme"><?= strip_tags(mb_substr($article['text'], 0, 100, 'utf-8')) . '...' ?>
@@ -34,3 +34,46 @@
     <?php } ?>
   </div>
 </section>
+
+<?php
+$i = 0;
+$articlesByCategories = $mainPageData->getArticlesByCategories();
+foreach ($articlesByCategories as $articlesByCategory) {
+  $toCategoryUrl = '/' . $urls['articles'] . '/' . $articlesByCategory[0]['category_url'];
+  ?>
+  <section class="main-content">
+    <div class="section__header">
+      <a href="<?= $toCategoryUrl ?>" class="section__headline content__button_grey-theme"><?= $articlesByCategory[0]['category_title'] ?></a>
+      <a href="<?= $toCategoryUrl ?>" class="main-content__header-button content__button_grey-theme">все записи</a>
+    </div>
+    <div class="main-content__articles-container">
+      <?php
+      $i = 0;
+      foreach ($articlesByCategory as $article) {
+        $toArticleUrl = '/' . $urls['articles'] . '/' . $article['url'];
+        ?>
+        <div class="main-content__article-preview-container
+          <?php if ($i++ <= 1)
+            echo ' main-content__article-preview-container-first-row';
+          ?>
+          ">
+          <div class="article-preview">
+            <a href="<?= $toArticleUrl ?>" class="article-preview__image-container" style="background-image: url('<?= $urlToImages . $article['image'] ?>');">
+            </a>
+            <div class="article-preview__information-container">
+              <a href="<?= $toArticleUrl ?>" class="article-preview__headline article-preview__interactive-button"><?= $article['title'] ?>
+              </a>
+              <div class="article-preview__category article-preview__interactive-button-container">
+                <span class="article-preview__category">категория:</span>
+                <a href="<?= '/' . $urls['articles'] . '/' . $article['category_url'] ?>" class="article-preview__category article-preview__interactive-button"><?= $article['category_title'] ?>
+                </a>
+              </div>
+              <a href="<?= $toArticleUrl ?>" class="article-preview__text content__button_grey-theme"><?= strip_tags(mb_substr($article['text'], 0, 100, 'utf-8')) . '...' ?>
+              </a>
+            </div>
+          </div>
+        </div>
+      <?php } ?>
+    </div>
+  </section>
+<?php } ?>
